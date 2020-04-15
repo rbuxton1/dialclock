d4b4f64b0ae3ee804d12d1fa207b54c2a81f1621
Ryan <rbuxton1@gmail.com>
2020-04-15 17:11:11 -0600

    oh ha ha it was me not the computer

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", funtion(req, res){
+app.get("/", function(req, res){
   res.render("index");
 });
 
