9dba4f78a33d9b31b23501c56a8e76aae95b6c2d
Ryan <rbuxton1@gmail.com>
2020-04-15 11:51:03 -0600

    uh oh

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const GPIO = require("pigpio").Gpio;
+
+//Servos
+//hours -> 14
+//minutes -> 15
+//seconds -> 18
+const hourServo = new GPIO(14, {mode: GPIO.OUTPUT});
+const minutesServo = new GPIO(15, {mode: GPIO.OUTPUT});
+const secondsServo = new GPIO(18, {mode: GPIO.OUTPUT});
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.listen(80, function(){ console.log("Listening on port 3000!"); });
+app.get("/", funtion(req, res){
+  res.render("index");
+});
+
+app.post("/setHours", function(req, res){
+  hourServo.servoWrite(req.body.hourServo);
+  res.redirect("/");
+});
+app.post("/setMinutes", function(req, res){
+  minutesServo.servoWrite(req.body.minutesServo);
+  res.redirect("/");
+});
+app.post("/setSeconds", function(req, res){
+  seconds.servoWrite(req.body.secondsServo);
+  res.redirect("/");
+});
+
+app.listen(8080, function(){ console.log("Listening on port 3000!"); });
