cfe5b9b34c4ecd62e3252e2b80addd2e3d21943f
Ryan <rbuxton1@gmail.com>
2020-04-15 20:25:53 -0600

    whole buncha stuff

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const Gpio = require("pigpio").Gpio;
+const fs = require('fs');
+
+//config data
+var configFile = fs.readFileSync("config.json");
+var config = JSON.parse(configFile);
 
 //Servos
 //hours -> 14
@@ -14,10 +19,6 @@ const minitesLight = new Gpio(8, {mode: Gpio.OUTPUT});
 const secondsServo = new Gpio(18, {mode: Gpio.OUTPUT});
 const secondsLight = new Gpio(7, {mode: Gpio.OUTPUT});
 
-//positions
-var left = 2200;
-var right = 700;
-
 //Math
 const scale = (num, in_min, in_max, out_min, out_max) => {
   return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
@@ -25,6 +26,7 @@ const scale = (num, in_min, in_max, out_min, out_max) => {
 
 //modes
 var clockMode = true;
+var secondsDir = false;
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -48,15 +50,53 @@ app.post("/set/seconds/pos", function(req, res){
 });
 
 app.post("/set/hours/bl", function(req, res){
+  config.hours.bl = req.body.bl;
+  fs.writeFileSync('config.json', config);
   hourLight.pwmWrite(req.body.bl);
   res.redirect("/");
 });
 app.post("/set/minutes/bl", function(req, res){
+  config.minutes.bl = req.body.bl;
+  fs.writeFileSync('config.json', config);
   minutesLight.pwmWrite(req.body.bl);
   res.redirect("/");
 });
 app.post("/set/minutes/bl", function(req, res){
+  config.seconds.bl = req.body.bl
+  fs.writeFileSync('config.json', config);
   secondsLight.pwmWrite(req.body.bl);
+  res.redirect("/");
+});
+
+app.post("/set/hours/left", function(req, res){
+  config.hours.left = req.body.left;
+  fs.writeFileSync('config.json', config);
+  res.redirect("/");
+});
+app.post("/set/minutes/left", function(req, res){
+  config.minutes.left = req.body.left;
+  fs.writeFileSync('config.json', config);
+  res.redirect("/");
+});
+app.post("/set/seconds/left", function(req, res){
+  config.seconds.left = req.body.left;
+  fs.writeFileSync('config.json', config);
+  res.redirect("/");
+});
+
+app.post("/set/hours/right", function(req, res){
+  config.hours.right = req.body.right;
+  fs.writeFileSync('config.json', config);
+  res.redirect("/");
+});
+app.post("/set/minutes/right", function(req, res){
+  config.minutes.right = req.body.right;
+  fs.writeFileSync('config.json', config);
+  res.redirect("/");
+});
+app.post("/set/seconds/right", function(req, res){
+  config.seconds.right = req.body.right;
+  fs.writeFileSync('config.json', config);
   res.redirect("/");
 });
 
@@ -71,9 +111,9 @@ app.listen(8080, function(){ console.log("Listening on port 8080!"); });
 setInterval(() => {
   if(clockMode){
     var date = new Date();
-    var hPos = left - scale(date.getHours(), 0, 23, right, left);
-    var mPos = left - scale(date.getMinutes(), 0, 59, right, left);
-    var sPos = left - scale(date.getSeconds(), 0, 59, right, left);
+    var hPos = left - scale(date.getHours(), 0, 23, config.hours.right, config.hours.left);
+    var mPos = left - scale(date.getMinutes(), 0, 59, config.minutes.right, config.hours.left);
+    var sPos = left - scale(date.getSeconds(), 0, 59, config.seconds.right, config.seconds.left);
 
     hourServo.servoWrite(Math.floor(hPos) + right);
     minutesServo.servoWrite(Math.floor(mPos) + right);
