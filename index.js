8bf76ed78c62161260e2cefd3e82671dd7706b71
Ryan <rbuxton1@gmail.com>
2020-04-15 21:46:16 -0600

    maybe init too much?

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const fs = require('fs');
 //config data
 var configFile = fs.readFileSync("config.json");
 var config = JSON.parse(configFile);
+var date = new Date();
 
 //Servos
 //hours -> 14
@@ -116,10 +117,9 @@ app.listen(8080, function(){ console.log("Listening on port 8080!"); });
 //clocking here
 setInterval(() => {
   if(clockMode){
-    var date = new Date();
-    var hPos = config.hours.left - Math.abs(Math.floor(scale(date.getHours(), 0, 24, config.hours.right, config.hours.left)));
-    var mPos = config.minutes.left - Math.abs(Math.floor(scale(date.getMinutes(), 0, 60, config.minutes.right, config.minutes.left)));
-    var sPos = config.seconds.left - Math.abs(Math.floor(scale(date.getSeconds(), 0, 60, config.seconds.right, config.seconds.left)));
+    let hPos = config.hours.left - Math.abs(Math.floor(scale(date.getHours(), 0, 24, config.hours.right, config.hours.left)));
+    let mPos = config.minutes.left - Math.abs(Math.floor(scale(date.getMinutes(), 0, 60, config.minutes.right, config.minutes.left)));
+    let sPos = config.seconds.left - Math.abs(Math.floor(scale(date.getSeconds(), 0, 60, config.seconds.right, config.seconds.left)));
 
     if(hPos > config.hours.right && hPos < config.hours.left) hourServo.servoWrite(hPos);
     else console.log("Erroneous hpos: " + hPos);
