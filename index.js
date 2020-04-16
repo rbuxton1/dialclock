278d68aed6aae2b8b7a869ba37553131d50d60ed
Ryan <rbuxton1@gmail.com>
2020-04-15 22:01:33 -0600

    probably actually gonna quit after this

--- index.js
+++ index.js
@@ -117,9 +117,9 @@ app.listen(8080, function(){ console.log("Listening on port 8080!"); });
 setInterval(() => {
   let currentTime = new Date();
   if(clockMode){
-    let hPos = config.hours.left - Math.abs(Math.floor(scale(currentTime.getHours(), 0, 24, config.hours.right, config.hours.left)));
-    let mPos = config.minutes.left - Math.abs(Math.floor(scale(currentTime.getMinutes(), 0, 60, config.minutes.right, config.minutes.left)));
-    let sPos = config.seconds.left - Math.abs(Math.floor(scale(currentTime.getSeconds(), 0, 60, config.seconds.right, config.seconds.left)));
+    let hPos = scale(currentTime.getHours(), 0, 24, config.hours.right, config.hours.left);
+    let mPos = scale(currentTime.getMinutes(), 0, 60, config.minutes.right, config.minutes.left);
+    let sPos = scale(currentTime.getSeconds(), 0, 60, config.seconds.right, config.seconds.left);
 
     if(hPos > config.hours.right && hPos < config.hours.left) hourServo.servoWrite(hPos);
     else console.log("Erroneous hpos: " + hPos);
