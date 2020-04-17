92696a487d0f953bdcee0ecc29f44dba2d5db618
Ryan <rbuxton1@gmail.com>
2020-04-17 00:04:50 -0600

    minutes 0 and 1 dont display and backlight control

--- index.js
+++ index.js
@@ -133,17 +133,17 @@ setInterval(() => {
     //let hPos = scale(currentTime.getHours(), 0, 24, config.hours.right, config.hours.left);
     //let mPos = scale(currentTime.getMinutes(), 0, 60, config.minutes.right, config.minutes.left);
     //let sPos = scale(currentTime.getSeconds(), 0, 60, config.seconds.right, config.seconds.left);
-    let hPos = config.hours.left - (((config.hours.left - config.hours.right) / 23) * currentTime.getHours());
-    let mPos = config.minutes.left - (((config.minutes.left - config.minutes.right) / 59) * currentTime.getMinutes());
-    let sPos =  config.seconds.left - (((config.seconds.left - config.seconds.right) / 59) * currentTime.getSeconds());
-    let sPosRev = config.seconds.left - (((config.seconds.left - config.seconds.right) / 59) * (59 - currentTime.getSeconds()));
+    let hPos = config.hours.left - (((config.hours.left - config.hours.right) / 24) * currentTime.getHours());
+    let mPos = config.minutes.left - (((config.minutes.left - config.minutes.right) / 60) * currentTime.getMinutes());
+    let sPos =  config.seconds.left - (((config.seconds.left - config.seconds.right) / 60) * (currentTime.getSeconds()));
+    let sPosRev = config.seconds.left - (((config.seconds.left - config.seconds.right) / 60) * (59 - (currentTime.getSeconds())));
 
     if(hPos >= config.hours.right && hPos <= config.hours.left) hourServo.servoWrite(Math.floor(hPos));
     else console.log("Erroneous hpos: " + hPos);
     if(mPos >= config.minutes.right && mPos <= config.minutes.left) minutesServo.servoWrite(Math.floor(mPos));
     else console.log("Erroneous mpos: " + mPos);
     if(sPos >= config.seconds.right && mPos <= config.seconds.left){
-      if(currentTime.getMinutes() % 2 == 0) secondsServo.servoWrite(Math.floor(sPos));
+      if((currentTime.getMinutes() + 1) % 2 == 0) secondsServo.servoWrite(Math.floor(sPos));
       else secondsServo.servoWrite(Math.floor(sPosRev));
     } else console.log("Erroneous spos: " + sPos);
   }
